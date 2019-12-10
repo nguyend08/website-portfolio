2004329032c9f0233eae99fac20c3cae0685f7d6
Daniel <dahnguyen@ucdavis.edu>
2019-12-10 14:57:05 -0800

    created terminal header and body.  Need to add text inside

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Title from './Components/Title'
+import About from './Components/About'
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <About/>
     </div>
   );
 }
